c47a9579a7a62bd0b6553fc78801ef7f091a337b
Germana-1 <germanagerriane@yahoo.com.br>
2023-03-07 14:23:25 -0300

    feat: include SIMPLE_JWT in settings.py

--- _project/settings.py
+++ _project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 import os
 import dotenv
 from django.core.management.utils import get_random_secret_key
@@ -117,6 +118,11 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=24),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=2),
+}
 
 
 # Internationalization
