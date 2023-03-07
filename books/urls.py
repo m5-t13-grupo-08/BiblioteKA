9aa9e31ee3f3e3ee8c8695137e34e53a7ec2959d
Germana-1 <germanagerriane@yahoo.com.br>
2023-03-07 17:14:00 -0300

    fix: loanView /Germana

--- books/urls.py
+++ books/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("books/<str:book_id>/", views.BookDetailView.as_view()),
     path("books/<str:book_id>/copy/", views.CopyView.as_view()),
     path("books/copy/<str:copy_id>/", views.CopyDetailView.as_view()),
-    path("books/copy/<str:copy_id>/loan/", LoanView.as_view()),
+    path("books/<str:book_id>/loan/", LoanView.as_view()),
     path("books/<str:book_id>/follow/", views.FollowBookView.as_view()),
 ]
