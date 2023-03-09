9ab660ca34053efeb868608ffb86d12526355086
Felipe Bulhões <felipebbulhoes@gmail.com>
2023-03-09 17:25:59 -0300

    feat: see user loans historic / Felipe

--- loans/views.py
+++ loans/views.py
@@ -1,14 +1,19 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveDestroyAPIView,
+    ListAPIView,
+)
+from rest_framework.views import APIView, Request, Response, status
 from .models import Loan
 from datetime import datetime
 from users.models import User
 from .serializers import LoanSerializer
 from django.shortcuts import get_object_or_404
-from users.models import User
 from books.models import Copy, Book
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.exceptions import NotFound
 from loans.permissions import LoanPermission
+from rest_framework.permissions import IsAuthenticated
 
 
 class LoanView(ListCreateAPIView):
@@ -36,19 +41,31 @@ class LoanView(ListCreateAPIView):
     def get_queryset(self):
         queryset = self.queryset.filter(user__id=self.kwargs.get("user_id"))
         return queryset
-    
+
+
 class LoanDetailView(RetrieveDestroyAPIView):
-    #authentication_classes = [JWTAuthentication]
-    #permission_classes = [LoanPermission]
-    
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
     serializer_class = LoanSerializer
     queryset = Loan.objects.all()
     lookup_url_kwarg = "loan_id"
-    
+
     def perform_destroy(self, instance):
-        
         loan = get_object_or_404(Loan, pk=self.kwargs.get("loan_id"))
-        
+
         instance.copy.is_free = True
         print(instance)
         instance.devolution_date = datetime.now()
+
+
+class LoanHistoricView(ListAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    serializer_class = LoanSerializer
+    queryset = Loan.objects.all()
+
+    def get_queryset(self):
+        queryset = self.queryset.filter(user=self.request.user)
+        return queryset
